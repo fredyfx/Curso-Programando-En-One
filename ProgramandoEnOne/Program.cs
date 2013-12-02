ba87f1ea32ee248db883d68000fac5151e71d96b
fredyfx <fredy_fx@outlook.com>
2013-12-01 20:26:24 -0800

    probando 3..2..

--- ProgramandoEnOne/Program.cs
+++ ProgramandoEnOne/Program.cs
@@ -9,6 +9,31 @@ namespace ProgramandoEnOne
     {
         static void Main(string[] args)
         {
+            //Iniciamos con todo el power!
+            
+            //Definimos las variables:
+            int numero;
+            string nombres;
+            decimal cantidadDinero;
+            //Para leer las variables:
+            // Variable = Lectura (se define el valor)
+            //se necesita un Console.ReadLine();
+            //
+            nombres = Console.ReadLine();
+            numero = int.Parse(Console.ReadLine());
+            cantidadDinero = decimal.Parse(Console.ReadLine());
+            //Estructuras Secuenciales
+
+            //Cabe mencionar que esto
+            //Estructuras Condicionales
+            //Decision
+            //Dobles
+            //Multiples
+            //Estructuras Repetitivas
+            //while
+            //dowhile
+            //for
+            //Fin de la primera temporada
         }
     }
 }
