2aa7d5f10aadc82e9a02d8e12533d8e9d70cb6d8
fredyfx <fredy_fx@outlook.com>
2013-12-01 20:35:25 -0800

    actualizando, al parecer funciona

--- ProgramandoEnOne/Program.cs
+++ ProgramandoEnOne/Program.cs
@@ -16,11 +16,15 @@ namespace ProgramandoEnOne
             string nombres;
             decimal cantidadDinero;
             //Para leer las variables:
-            // Variable = Lectura (se establece el valor)
+            //Variable = Lectura (se establece el valor)
             //se necesita un Console.ReadLine();
+            //Para decirle al usuario
             //
+            Console.WriteLine("Ingrese su nombre: ");
             nombres = Console.ReadLine();
+            Console.Write("Ingrese su edad: ");
             numero = int.Parse(Console.ReadLine());
+            Console.Write("Cuanto dinero tiene en el bolsillo?: ");
             cantidadDinero = decimal.Parse(Console.ReadLine());
             //Estructuras Secuenciales
 
